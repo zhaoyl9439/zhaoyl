ec453f71aac7015e9538a4ee6ca8644329dc2b73
zhaoyl9439 <zhaoyl9439@163.com>
2021-03-07 22:03:19 +0800

    线程唤醒##20210307

--- demo/src/main/java/com/example/demo/thread/WaitSet.java
+++ demo/src/main/java/com/example/demo/thread/WaitSet.java
@@ -11,6 +11,7 @@ public class WaitSet {
      * 1.所有的对象都会有一个wait set，用来存放调用了该对象wait方法之后进入block状态线程
      * 2.线程被notify之后，不一定立即得到执行
      * 3.线程从wait set中被唤醒顺序不一定是FIFO（顺序）
+     * 4.线程被唤醒后，必须重新获取锁
      * @param args
      */
     public static void main(String[] args) {
